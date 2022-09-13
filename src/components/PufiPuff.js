89c77ad40d400382eb64757f8a15dbc2f1b3da98
angela <angelabecerraforero@gmail.com>
2022-09-13 15:22:19 -0500

    Initial layout of the PufiPuff component is done, but the positions of each item need to be fixed.

--- src/components/PufiPuff.js
+++ src/components/PufiPuff.js
@@ -1,8 +1,20 @@
 import React from "react";
+import styles from './PufiPuff.module.css';
 
 function PufiPuff() {
     return (
-        <h3>Descripción del producto. Est es un texto simulado</h3>
+        <div className={styles.container}>
+            <div className={styles.description}>
+                <img src="https://i.imgur.com/RDOxQ7E.png" alt="onePuff"></img>
+                <h3>Pufi PUFF</h3>
+                <h4>Descripción del producto. Este es un texto simulado</h4>
+                <button> VER MÁS </button>
+            </div>
+            <div className={styles.imagePuff}>
+                <img src="https://i.imgur.com/g4I5DhT.png" alt="puff"></img>
+                <button>SHOP</button>
+            </div>
+        </div>
     );
 }
 
