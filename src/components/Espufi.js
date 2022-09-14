b8a07886ff631458258fba764c4fceb33d70e27b
angela <angelabecerraforero@gmail.com>
2022-09-13 20:55:07 -0500

    Initial layout of the Espufi component is done, but the positions of each item need to be fixed.

--- src/components/Espufi.js
+++ src/components/Espufi.js
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from './Espufi.module.css';
+import { useForm } from '@formspree/react';
 
 function Espufi() {
+
+    
     return (
         <div className={styles.container}>
             <h5>INSTAGRAM</h5>
@@ -10,7 +13,13 @@ function Espufi() {
             <h5>NEWSLETTER</h5>
             <h2>SUSCRIBETE</h2>
             <h6>Y enterante de todas las novedades</h6>
-            <input placeholder="Ingresa tu email -->"></input>
+            <form className={styles.form}>
+                <input id="email" name="email" type="email" placeholder="Ingresa tu email"></input>
+                <button>
+                    <img src="https://i.imgur.com/xdT2poi.png" alt="icon"></img>
+                </button>
+            </form>
+            
         </div>
     );
 }
